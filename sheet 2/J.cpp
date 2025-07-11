b8766952669b4bd4ad4409360339b219ef168dfc
Sadiaborsha19 <saadiiaa346@gmail.com>
2025-07-11 20:27:52 +0600

    Update J.cpp

--- sheet 2/J.cpp
+++ sheet 2/J.cpp
@@ -1,3 +1,8 @@
+1.// Problem name: Primes from 1 to n .
+2.// Course title: Competitive Programming Sessional
+3.// Course code: CCE-2310
+4.// Course teacher name: Mirza Raquib
+5.// Date: 05/04/25
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -17,6 +22,6 @@ int main()
         if(count == 2)
         {
             cout<<i<<" ";
-        }
-    }
+ Â Â Â Â Â Â Â }
+Â Â Â Â }
 }
