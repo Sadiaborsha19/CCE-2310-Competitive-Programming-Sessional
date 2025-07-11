05c587b76346a1ca80a026de2af999623b96af35
Sadiaborsha19 <saadiiaa346@gmail.com>
2025-07-11 20:31:47 +0600

    Update S.cpp

--- sheet 2/S.cpp
+++ sheet 2/S.cpp
@@ -1,3 +1,8 @@
+1.// Problem name: Sum of consecutive odd numbers .
+2.// Course title: Competitive Programming Sessional
+3.// Course code: CCE-2310
+4.// Course teacher name: Mirza Raquib
+5.// Date: 05/04/25
 #include<bits/stdc++.h>
 using namespace std;
 
