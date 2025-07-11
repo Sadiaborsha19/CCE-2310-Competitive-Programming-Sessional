6243cc99a8601d35a1d7f1345565ab99ee43fe92
Sadiaborsha19 <saadiiaa346@gmail.com>
2025-07-11 20:44:49 +0600

    Update K.cpp

--- sheet 3/K.cpp
+++ sheet 3/K.cpp
@@ -1,3 +1,8 @@
+1.// Problem name: sum digits.
+2.// Course title: Competitive Programming Sessional
+3.// Course code: CCE-2310
+4.// Course teacher name: Mirza Raquib
+5.// Date: 22/6/25
 #include<bits/stdc++.h>
 using namespace std;
 int main()
