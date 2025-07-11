ebaa05c0717062549c93020f632b446701cdb578
Sadiaborsha19 <saadiiaa346@gmail.com>
2025-07-11 20:24:39 +0600

    Update C.cpp

--- sheet 2/C.cpp
+++ sheet 2/C.cpp
@@ -1,8 +1,8 @@
-.// Problem name: Replacement
+.// Problem name: Even ,odd,positiveand negative.
 2.// Course title: Competitive Programming Sessional
 3.// Course code: CCE-2310
 4.// Course teacher name: Mirza Raquib
-5.// Date: 5/5/25
+5.// Date: 5/4/25
 #include<bits/stdc++.h>
 using namespace std;
 int main()
