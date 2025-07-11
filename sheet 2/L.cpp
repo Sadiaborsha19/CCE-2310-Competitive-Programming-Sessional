db793ac6e02d38a6472da298e01aab3b15dc636a
Sadiaborsha19 <saadiiaa346@gmail.com>
2025-07-11 20:28:49 +0600

    Update L.cpp

--- sheet 2/L.cpp
+++ sheet 2/L.cpp
@@ -1,3 +1,8 @@
+1.// Problem name:  Gcd.
+2.// Course title: Competitive Programming Sessional
+3.// Course code: CCE-2310
+4.// Course teacher name: Mirza Raquib
+5.// Date: 05/04/25
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -15,5 +20,5 @@ int main()
         }
 
     }
-     cout<<gcd;
+ Â Â Â Â cout<<gcd;
 }
