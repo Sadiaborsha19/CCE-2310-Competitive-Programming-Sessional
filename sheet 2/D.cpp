9db21787e431ff377b2502fdcb877f6ae501e658
Sadiaborsha19 <saadiiaa346@gmail.com>
2025-07-11 20:25:13 +0600

    Update D.cpp

--- sheet 2/D.cpp
+++ sheet 2/D.cpp
@@ -1,8 +1,8 @@
-.// Problem name: (Position in array)
+.// Problem name: fixed password
 2.// Course title: Competitive Programming Sessional
 3.// Course code: CCE-2310
 4.// Course teacher name: Mirza Raquib
-5.// Date: 5/5/25
+5.// Date: 5/04/25
 #include<bits/stdc++.h>
 using namespace std;
 int main()
